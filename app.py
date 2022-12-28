9008c0543a4b0a785f6d25ad078a07b876898982
Bartosz Nawrocki <nawrockibartoszwro@gmail.com>
2022-12-28 13:45:58 +0100

    chapter 3 created 2 classes.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
+
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -35,6 +43,26 @@ def url_variables(name: str, age: int):
     else:
         return jsonify(message=f'welcome {name} you are old enough!')
 
+
+# database models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    plante_name = Column(String)
+    plante_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 if __name__ == '__main__':
     app.run()
